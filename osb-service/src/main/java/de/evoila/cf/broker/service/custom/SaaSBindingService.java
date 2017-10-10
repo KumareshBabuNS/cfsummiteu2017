4ca088c34306c7198fbdb2a14ba3967769be5067
Yannic Remmet <yremmet@evoila.de>
2017-10-10 15:26:11 +0200

    feat(BindingService): implemented saas binding service

--- osb-service/src/main/java/de/evoila/cf/broker/service/custom/SaaSBindingService.java
+++ osb-service/src/main/java/de/evoila/cf/broker/service/custom/SaaSBindingService.java
@@ -3,21 +3,42 @@ package de.evoila.cf.broker.service.custom;
 import de.evoila.cf.broker.exception.*;
 import de.evoila.cf.broker.model.ServiceInstanceBinding;
 import de.evoila.cf.broker.model.ServiceInstanceBindingResponse;
+import de.evoila.cf.broker.repository.BindingRepository;
+import de.evoila.cf.broker.repository.ServiceInstanceRepository;
 import de.evoila.cf.broker.service.BindingService;
+import de.evoila.cf.broker.service.CatalogService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class SaaSBindingService implements BindingService{
+    @Autowired
+    CatalogService catalogService;
+    @Autowired
+    ServiceInstanceRepository repository;
+    @Autowired
+    BindingRepository bindingRepository;
+
+
     @Override
     public ServiceInstanceBindingResponse createServiceInstanceBinding (String bindingId, String instanceId, String serviceId, String planId, boolean generateServiceKey, String route) throws ServiceInstanceBindingExistsException, ServiceBrokerException, ServiceInstanceDoesNotExistException, ServiceDefinitionDoesNotExistException {
-        return null;
+        String serviceDefinitionId = repository.getServiceInstance(instanceId).getServiceDefinitionId();
+        Map<String, Object> credentials = catalogService.getServiceDefinition(serviceDefinitionId).getMetadata();
+        ServiceInstanceBinding serviceBinding = new ServiceInstanceBinding(bindingId,instanceId,credentials,null);
+        bindingRepository.addInternalBinding(serviceBinding);
+        return new ServiceInstanceBindingResponse(serviceBinding);
     }
 
     @Override
     public ServiceInstanceBinding getServiceInstanceBinding (String id) {
-        return null;
+        ServiceInstanceBinding binding = bindingRepository.findOne(id);
+        return binding;
     }
 
     @Override
     public void deleteServiceInstanceBinding (String id) throws ServiceBrokerException, ServerviceInstanceBindingDoesNotExistsException {
-
+        bindingRepository.deleteBinding(id);
     }
 }
